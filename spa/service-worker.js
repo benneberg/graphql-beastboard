44abc41848101190dc9f793bb822eae7c4d0f2ec
Lukas B <benneberg@users.noreply.github.com>
2025-12-09 01:21:59 +0100

    Add service worker for caching assets

---
+++ spa/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'beastboard-cache-v1';
+
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
