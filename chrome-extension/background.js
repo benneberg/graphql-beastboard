d2efca51083fef149f2ec2d29dd9096d8e05c8df
Lukas B <benneberg@users.noreply.github.com>
2025-12-09 04:19:56 +0100

    Add background script for action and command handling

---
+++ chrome-extension/background.js
@@ -0,0 +1,12 @@
+// ===== background.js =====
+chrome.action.onClicked.addListener((tab) => {
+  chrome.tabs.sendMessage(tab.id, { action: 'toggle' });
+});
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'toggle-dashboard') {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'toggle' });
+    });
+  }
+});
